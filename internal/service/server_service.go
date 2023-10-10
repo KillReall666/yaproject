ebf7f774467e7bfba8a2c0caf1aa89893b4d4ff2
KillReal <codewarrior666@mail.ru>
2023-10-10 21:03:38 +0300

    iter9

---
+++ internal/service/server_service.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"github.com/KillReall666/yaproject/internal/logger"
+	"github.com/KillReall666/yaproject/internal/model"
+	"github.com/KillReall666/yaproject/internal/storage"
+	"github.com/jmoiron/sqlx"
+)
+
+type Service struct {
+	repository *storage.MemStorage
+	log        *logger.Logger
+	db         *sqlx.DB
+}
+
+func NewService(repo *storage.MemStorage, log *logger.Logger, connect *sqlx.DB) *Service {
+	return &Service{
+		repository: repo,
+		log:        log,
+		db:         connect,
+	}
+}
+
+func (s *Service) SaveMetrics(request *model.Metrics) error {
+	if request.Counter != nil {
+		s.repository.CountSetter(request.Name, *request.Counter)
+		return nil
+	}
+
+	if request.Gauge != nil {
+		s.repository.GaugeSetter(request.Name, *request.Gauge)
+		return nil
+	}
+	return nil
+}
+
+func (s *Service) GetFloatMetrics(request *model.Metrics) (float64, error) {
+	value, err := s.repository.GaugeGetter(request.Name)
+	return value, err
+
+}
+
+func (s *Service) GetCountMetrics(request *model.Metrics) (int64, error) {
+	value, err := s.repository.CountGetter(request.Name)
+	return value, err
+}
+
+func (s *Service) PrintForHTML() string {
+	htmlPage := s.repository.GetAllMetrics()
+	return htmlPage
+}
+
+func (s *Service) MetricsPrint() {
+	s.repository.Print()
+}
+
+func (s *Service) LogInfo(args ...interface{}) {
+	s.log.Sugar.Info(args)
+}
