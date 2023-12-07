d555f50734b4bee9ce68e896be3a197cfda854f9
KillReall666 <codewarrior666@mail.ru>
2023-12-07 11:02:15 +0300

    Revert "Iter4 (#4)"
    This reverts commit 03209d17c8fd9849f80094268b049bcd2d0ccbed.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,21 +1,3 @@
 package main
 
-import (
-	"log"
-
-	agent "github.com/KillReall666/yaproject/internal/client"
-	metrics2 "github.com/KillReall666/yaproject/internal/client/metrics"
-	"github.com/KillReall666/yaproject/internal/config"
-)
-
-func main() {
-	cfg := config.LoadAgentConfig()
-	gms := metrics2.NewGaugeMetricsStorage()
-	cli := agent.NewClient(cfg, gms)
-
-	err := cli.Run()
-	if err != nil {
-		log.Fatalf("client died on error: %v", err)
-	}
-
-}
+func main() {}
--- internal/model/dto.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Metrics struct {
-	Name    string
-	Counter *int64
-	Gauge   *float64
-}
